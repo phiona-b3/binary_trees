a72fe0e32edc59b0cecb38d39c48841dbb33b754
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-09-27 11:19:46 -0700

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,3 +25,20 @@ level_right += 1;
 }
 return (level_right > level_left ? level_right : level_left);
 }
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return:  balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+int left, right;
+
+if (tree == NULL)
+	return (0);
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
+return (left - right);
+}
